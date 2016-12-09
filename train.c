fb8bfb58a814b34302ff12841add0ec9946973eb
saurabhschheda <saurabh.s.chheda@gmail.com>
2016-12-09 22:13:49 +0000

    Removed hardcoded values

--- train.c
+++ train.c
@@ -48,56 +48,16 @@ void main()
     printf("=============== TRAIN OPTIMISATION ================\n");
 
     //Enter number of stations
-    //noOfStations = getStations();
-    noOfStations = 13;
-    displayStations(noOfStations);
-    printf("\nSource Station: 0\n\n");
-
+    noOfStations = getStations();
+    
     //Create Station Map
     printf("=============== CREATE STATION MAP ================\n");
-    //createStationMap(noOfStations, map);
-    addEdge(map, 0, 1, 7);
-    addEdge(map, 1, 2, 2);
-    addEdge(map, 2, 5, 9);
-    addEdge(map, 2, 4, 8);
-    addEdge(map, 1, 3, 5);    
-    addEdge(map, 1, 6, 1);
-    addEdge(map, 6, 7, 11);   
-    addEdge(map, 7, 10, 19);    
-    addEdge(map, 6, 8, 3);    
-    addEdge(map, 10, 8, 9);
-    addEdge(map, 8, 9, 6);
-    addEdge(map, 9, 11, 4);
-    addEdge(map, 9, 12, 5);
+    createStationMap(noOfStations, map);
     displayStationMap(noOfStations, map);
 
     //Enter Bogey Details
     printf("=============== ENTER BOGEY DETAILS ===============\n");
-    //enterBogeyDetails(train, noOfStations);
-    for (i = 0; i < 4; ++i)
-    {
-        train[i].bogeyID = i + 1;
-        train[i].itemID = i + 1;
-        train[i].station = 5;
-    }
-    for (i = 4; i < 8; ++i)
-    {
-        train[i].bogeyID = i + 1;
-        train[i].itemID = i + 1;
-        train[i].station = 7;
-    }
-    for (i = 8; i < 12; ++i)
-    {
-        train[i].bogeyID = i + 1;
-        train[i].itemID = i + 1;
-        train[i].station = 10;
-    }
-    for (i = 12; i < 15; ++i)
-    {
-        train[i].bogeyID = i + 1;
-        train[i].itemID = i + 1;
-        train[i].station = 12;
-    }
+    enterBogeyDetails(train, noOfStations);
     displayBogeyDetails(train);
 
     //Route
